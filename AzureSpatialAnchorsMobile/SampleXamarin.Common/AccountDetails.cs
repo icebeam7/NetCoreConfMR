215e6ad0efa981148227bd6570f9cb3f2f8ba08b
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2022-04-01 08:04:36 +0200

    Add project files.

---
+++ AzureSpatialAnchorsMobile/SampleXamarin.Common/AccountDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace SampleXamarin
+{
+    public static class AccountDetails
+    {
+        public const string SpatialAnchorsAccountId = "944f6a46-daa4-46ea-a1fd-2298e98020cb";
+
+        public const string SpatialAnchorsAccountKey = "snnl3s1OImSWvTgPZzC0lQUwxlOfsY7lc2XOveNL7qA=";
+
+        public const string SpatialAnchorsAccountDomain = "northeurope.mixedreality.azure.com";
+
+        public const string AnchorSharingServiceUrl = "Set me";
+    }
+}
